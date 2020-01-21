5d32ff15e1e6e8ca2b2ad6174b980d4ab48aaa2a
Jenny Nguy <j.nguy809@gmail.com>
2020-01-21 01:16:39 -0500

    create react app

---
+++ car-price-calculator/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section>
+        Base Price: $24,999
+      </section>
+      <section>
+        
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ car-price-calculator/src/components/AddOns.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Checkbox from './Checkbox';
+
+export default AddOns = () => {
+  return (
+    <div className="add-ons-form">
+      <label>
+        <input 
+          type="checkbox"
+          name="label"
+          checked=
+        />
+      </label>
+    </div>
+  )
+}
